3f4bc75534c4965850bdd5ff79034465a026b763
Albin Ljunghusen <albinljunghusen@gmail.com>
2023-11-03 18:21:43 +0100

    Day4

--- typescript/2019/day2.test.ts
+++ typescript/2019/day2.test.ts
@@ -11,7 +11,39 @@ const input = await Bun.file(
   'day1.input.txt'
 ).text()
 
-const part1 = (input: string) => {}
+const part1 = (input: string) => {
+  const [from, to] = input
+    .split('-')
+    .map((str) => parseInt(str))
+
+  let test = from
+  let numPass = 0
+  do {
+    const str = test.toString()
+    if (
+      Array.from(str).some(
+        (c, i, arr) =>
+          c === arr[i + 1] &&
+          (i === 5 ||
+            c !== arr[i + 2]) &&
+          (i === 0 || c !== arr[i - 1])
+      ) &&
+      !Array.from(str).some(
+        (c, i, arr) =>
+          i != 0 && c < arr[i - 1]
+      )
+    ) {
+      console.log(str)
+      numPass++
+    }
+
+    test++
+  } while (test <= to)
+
+  return numPass
+}
+
+// 838
 
 test('part1', () => {
   const a = part1('256310-732736')
