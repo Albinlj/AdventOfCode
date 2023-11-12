511a19801c1c955634fd3d2bac247e5135be7cc8
Albin Ljunghusen <albinljunghusen@gmail.com>
2023-11-12 16:03:46 +0100

    fudge day10

--- typescript/2019/day10.test.ts
+++ typescript/2019/day10.test.ts
@@ -13,7 +13,7 @@ const part1 = (input: string) => {
   for (let x = 0; x < width; x++) {
     for (let y = 0; y < height; y++) {
       const ch = lines[y][x]
-      if (ch === '#')
+      if (ch !== '.')
         asteroids.push([x, y])
     }
   }
@@ -58,7 +58,10 @@ const part1 = (input: string) => {
   return maxVis
 }
 
-const part2 = (input: string) => {
+const part2 = (
+  input: string,
+  th: number
+) => {
   const lines = input.split('\n')
   const height = lines.length
   const width = lines.at(0)!.length
@@ -102,7 +105,9 @@ const part2 = (input: string) => {
         relY
       )
       visible.add(
-        relX / gcd + ',' + relY / gcd
+        relX / Math.abs(gcd) +
+          ',' +
+          relY / Math.abs(gcd)
       )
     }
 
@@ -130,14 +135,11 @@ const part2 = (input: string) => {
       relY
     )
 
-    asteroid[3] =
-      (Math.atan2(
-        relY / gcd,
-        relX / gcd
-      ) +
-        Math.PI / 2 +
-        2 * Math.PI) %
-      (2 * Math.PI)
+    asteroid[3] = radToDeg(
+      (Math.atan2(relY, relX) +
+        2.5 * Math.PI) %
+        (2 * Math.PI)
+    )
   }
 
   asteroids.sort(
@@ -145,52 +147,71 @@ const part2 = (input: string) => {
       [x1, y1, distance1, ang1],
       [x2, y2, distance2, ang2]
     ) =>
-      (ang1 - ang2) * 1000 +
+      (ang1 - ang2) * 100000 +
       (distance1 - distance2)
   )
-  const anglesToAsteroid = new Map<
-    number,
-    number[]
-  >()
+
+  console.log(asteroids)
 
   const sortedAsteroids = collectBy(
     (a) => a[3],
     asteroids
   )
 
-  // console.log(sortedAsteroids)
-
   let i = 0
   let blown = 0
+
+  // console.log('------')
+  // console.log(
+  //   sortedAsteroids.slice(95, 105)
+  // )
+
   while (true) {
     const currAngleAstroids =
       sortedAsteroids[
         i % sortedAsteroids.length
       ]
+
     if (i < 5) {
       // console.log(currAngleAstroids)
     }
+
     const toBlow =
       currAngleAstroids.shift()
 
+    // if (toBlow[3] === 0) {
+    //   console.log(toBlow)
+    //   console.log(i, blown)
+    // }
+
     if (toBlow) {
-      if (
-        toBlow[0] === laserPos[0] &&
-        toBlow[1] === laserPos[1]
-      ) {
-        console.log('ASIDJIODJQIOWE')
-      } else {
-        blown++
-      }
+      // if (
+      //   toBlow[3] > Math.PI / 2 &&
+      //   toBlow[3] < Math.PI
+      // ) {
+      //   console.log(toBlow)
+      // }
+
+      // if (
+      //   toBlow[3] > Math.PI &&
+      //   toBlow[3] < Math.PI * 1.5
+      // ) {
+      //   console.log(toBlow)
+      // }
+      // console.log(toBlow)
+      const ang = toBlow[3]
+      blown++
+      // console.log(ang)
 
       // console.log(toBlow)
-      if (blown === 51) {
-        console.log(currAngleAstroids)
+      // if (blown === th) {
+      if (blown === 319) {
         return [toBlow[0], toBlow[1]]
       }
     }
 
     i++
+    // console.log(i, blown)
   }
 
   return sortedAsteroids[0]
@@ -208,20 +229,47 @@ const example2 = await Bun.file(
   'day10.example2.txt'
 ).text()
 
-test('part1', () => {
-  const a = part1(example1)
-  expect(a).toBe(8)
+// test('part1', () => {
+//   const a = part1(example1)
+//   expect(a).toBe(8)
 
-  const b = part1(example2)
-  expect(b).toBe(210)
+//   const b = part1(example2)
+//   expect(b).toBe(210)
 
-  const c = part1(input)
-  expect(c).toBe(274)
-})
+//   const c = part1(input)
+//   expect(c).toBe(274)
+// })
 
 test('part2', () => {
-  const b = part2(example2)
-  expect(b).toEqual([8, 2])
+  expect(part2(example2, 1)).toEqual([
+    11, 12,
+  ])
+  expect(part2(example2, 2)).toEqual([
+    12, 1,
+  ])
+  expect(part2(example2, 3)).toEqual([
+    12, 2,
+  ])
+  expect(part2(example2, 10)).toEqual([
+    12, 8,
+  ])
+
+  expect(part2(example2, 20)).toEqual([
+    16, 0,
+  ])
+  expect(part2(example2, 50)).toEqual([
+    16, 9,
+  ])
+  expect(part2(example2, 100)).toEqual([
+    10, 6,
+  ])
+
+  expect(part2(example2, 199)).toEqual([
+    9, 6,
+  ])
+  expect(part2(example2, 200)).toEqual([
+    8, 2,
+  ])
 
   // const c = part2(input)
   // expect(c).toBe(274)
@@ -240,3 +288,6 @@ function greatestCommonDivisor(
     return Math.abs(a)
   }
 }
+
+const radToDeg = (rad: number) =>
+  (rad / Math.PI) * 180
