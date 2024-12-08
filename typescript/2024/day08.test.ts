77883a38c91d431ce005da23868ac9d93db16d49
Albin Ljunghusen <albinljunghusen@gmail.com>
2024-12-08 13:18:10 +0100

    Day 08 finished

--- typescript/2024/day08.test.ts
+++ typescript/2024/day08.test.ts
@@ -1,6 +1,7 @@
 import { expect, test } from 'bun:test'
+import chalk from 'chalk'
 
-const input = await Bun.file('day07.input.txt').text()
+const input = await Bun.file('day08.input.txt').text()
 
 const example = `
 ............
@@ -19,32 +20,33 @@ const example = `
 
 test('day07', () => {
   expect(part1(example)).toBe(14)
-  // expect(part1(input)).toBe(14)
+  expect(part1(input)).toBe(301)
+  expect(part2(example)).toBe(34)
+  expect(part2(input)).toBe(1019)
 })
 
 const part1 = (input: string) => {
   const grid = input.trim().split('\n')
-
   const width = grid[0].length
   const height = grid.length
 
-  console.log({ width, height })
-
   const towers = grid.flatMap((line, y) =>
     line
+      .trim()
       .split('')
       .map((ch, x) => ({ x, y, ch }))
-      .filter(({ ch }) => ch !== '.')
+      .filter(({ ch }) => ch !== '.' && ch !== ' ')
   )
 
   const frequencies = new Set([...towers.map(({ ch }) => ch)])
-  const antiNodes = new Set()
+  const antiNodes = new Set('')
 
   for (const frequency of frequencies) {
     const towersOfFrequency = towers.filter(({ ch }) => ch === frequency)
 
-    for (let i = 0; i < towersOfFrequency.length - 1; i++) {
-      for (let j = i + 1; j < towersOfFrequency.length; j++) {
+    for (let i = 0; i < towersOfFrequency.length; i++) {
+      for (let j = 0; j < towersOfFrequency.length; j++) {
+        if (i === j) continue
         const a = towersOfFrequency[i]
         const b = towersOfFrequency[j]
 
@@ -54,17 +56,56 @@ const part1 = (input: string) => {
         const cx = b.x - deltaX
         const cy = b.y - deltaY
 
-        const dx = a.x + deltaX
-        const dy = a.y + deltaY
-
-        console.log({ a, b, deltaX, deltaY, cx, cy, dx, dy })
+        // console.log({ a, b, deltaX, deltaY, cx, cy })
 
         if (cx < width && cx >= 0 && cy < height && cy >= 0) {
-          antiNodes.add(cx + ',' + cy)
+          antiNodes.add('' + cx + ',' + cy)
         }
+      }
+    }
+  }
 
-        if (dx < width && dx >= 0 && dy < height && dy >= 0) {
-          antiNodes.add(dx + ',' + dy)
+  return antiNodes.size
+}
+
+const part2 = (input: string) => {
+  const grid = input.trim().split('\n')
+  const width = grid[0].length
+  const height = grid.length
+
+  const towers = grid.flatMap((line, y) =>
+    line
+      .trim()
+      .split('')
+      .map((ch, x) => ({ x, y, ch }))
+      .filter(({ ch }) => ch !== '.' && ch !== ' ')
+  )
+
+  const frequencies = new Set([...towers.map(({ ch }) => ch)])
+  console.log(frequencies)
+  const antiNodes = new Set('')
+
+  for (const frequency of frequencies) {
+    const towersOfFrequency = towers.filter(({ ch }) => ch === frequency)
+
+    for (let i = 0; i < towersOfFrequency.length; i++) {
+      for (let j = 0; j < towersOfFrequency.length; j++) {
+        if (i === j) continue
+        const a = towersOfFrequency[i]
+        const b = towersOfFrequency[j]
+
+        const deltaX = a.x - b.x
+        const deltaY = a.y - b.y
+
+        let nodeX = b.x
+        let nodeY = b.y
+
+        // console.log({ a, b, deltaX, deltaY, cx, cy })
+
+        while (nodeX < width && nodeX >= 0 && nodeY < height && nodeY >= 0) {
+          antiNodes.add('' + nodeX + ',' + nodeY)
+          nodeX += deltaX
+          nodeY += deltaY
         }
       }
     }
